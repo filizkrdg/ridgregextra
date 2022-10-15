7e6c2c770ce2a7ce9210950ec01a7c1dfb321586
olgnaydn <olgunaydinn@gmail.com>
2022-10-15 12:54:07 +0200

    first version of the package built

---
+++ R/vif_k.R
@@ -0,0 +1,27 @@
+vif_k<- function(x,y,a,b) {
+  
+  if ((a>b) | (a<0) | (b>1)){
+    print('Wrong input, please try again!')
+    
+    return(vif.k)
+    
+  } else {
+    
+    k1 <- seq(a,b,length=11)
+    k <- as.matrix(k1)
+    
+    v <- matrix(0,11,dim(x)[2])
+    
+    for (i in 1:11) {
+      
+      VIF <- ridge_reg(x,y,k[i])$VIF
+      diagVIF <- as.matrix(diag(VIF))
+      tdiagVIF <- t(diagVIF)
+      
+      v[i,] <- tdiagVIF[1,]
+      
+    }
+    # Instead of print, use return
+    print(cbind(k,v))
+  }
+}
