fe589ee7fd1da048731960e228bc181b9c54fbf0
olgnaydn <olgunaydinn@gmail.com>
2022-10-15 14:53:51 +0200

    plot added

--- R/vif_k.R
+++ R/vif_k.R
@@ -12,16 +12,28 @@ vif_k<- function(x,y,a,b) {
     
     v <- matrix(0,11,dim(x)[2])
     
-    for (i in 1:11) {
+    all_results <- NULL
+    for (i in 1:11) 
+      {
       
       VIF <- ridge_reg(x,y,k[i])$VIF
       diagVIF <- as.matrix(diag(VIF))
       tdiagVIF <- t(diagVIF)
-      
       v[i,] <- tdiagVIF[1,]
-      
     }
-    # Instead of print, use return
-    print(cbind(k,v))
+    
+    k_v <- as.data.frame(cbind(k,v))
+    colnames(k_v) <- c("k",paste("VIF.", colnames(x),sep=""))
+  
+    bar_plot <- plot_ly(type = 'bar')
+
+    for(i in 2:dim(k_v)[2])
+    {
+      dfk <- data.frame(y=k_v[,i], k=k_v$k)
+      bar_plot <- bar_plot %>% add_trace(data = dfk, y = ~y,x = ~k,name = colnames(k_v)[i])
+    }
   }
+  bar_plot <- bar_plot %>% layout(yaxis = list(title = 'k'), barmode = 'group')
+  print(bar_plot)
+  return(k_v)
 }
