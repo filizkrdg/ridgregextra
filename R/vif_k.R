f3a0aa9065e1c721967520fb2e0adea00a4f600b
olgnaydn <olgunaydinn@gmail.com>
2022-10-16 21:13:41 +0200

    trace names for VIF graph fixed

--- R/vif_k.R
+++ R/vif_k.R
@@ -23,17 +23,21 @@ vif_k<- function(x,y,a,b) {
     }
     
     k_v <- as.data.frame(cbind(k,v))
+    
+    #TODO: Fix colname assignment. Should happen inside of loop
     colnames(k_v) <- c("k",paste("VIF.", colnames(x),sep=""))
   
     bar_plot <- plot_ly(type = 'bar')
 
     for(i in 2:dim(k_v)[2])
     {
+      trace_name <- paste("VIF.x",i-1,sep="")
       dfk <- data.frame(y=k_v[,i], k=k_v$k)
-      bar_plot <- bar_plot %>% add_trace(data = dfk, y = ~y,x = ~k,name = colnames(k_v)[i])
+      bar_plot <- bar_plot %>% add_trace(data = dfk, y = ~y, x = ~k,name = trace_name)
     }
   }
-  bar_plot <- bar_plot %>% layout(yaxis = list(title = 'k'), barmode = 'group')
+  bar_plot <- bar_plot %>% layout(yaxis = list(title = 'VIF'), barmode = 'group')
+  bar_plot <- bar_plot %>% layout(xaxis = list(title = 'k'), barmode = 'group')
   print(bar_plot)
   return(k_v)
 }
